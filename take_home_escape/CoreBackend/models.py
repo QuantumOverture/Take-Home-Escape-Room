187dc7d8d289cbcb4d43cc2812e94557025dbc49
ismaildude <38566647+ismaildude@users.noreply.github.com>
2020-11-07 18:37:16 -0800

    Update models.py

--- take_home_escape/CoreBackend/models.py
+++ take_home_escape/CoreBackend/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Solutions(models.Model):
+    button = models.CharField(max_length=300)
+    wires = models.CharField(max_length=300)
+    keypad = models.CharField(max_length=300)
+
+    button_model = models.CharField(max_length=300,default="null")
+    wires_model = models.CharField(max_length=300,default="null")
+    keypad_model = models.CharField(max_length=300,default="null")
+
+    created = models.DateTimeField(default=timezone.now)
+    led_color = models.CharField(max_length=300,default="null")
